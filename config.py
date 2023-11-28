a6eecd83643f01cbbe3eb5898f0185ca80ad1c78
GiovanniNicol <gnicoletti2004@gmail.com>
2023-11-28 17:01:25 +0100

    Second commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Google Maps API Key
 GOOGLE_MAPS_API_KEY = "your_actual_google_maps_api_key"
 
-# Configuration for other APIs or settings
+# Configuration for other APIs or settings, you should find some APIs for the mobility apps that we discussed
 SBB_API_KEY = "your_actual_sbb_api_key"
 
 SCOOTER_API_KEY = "your_actual_scooter_api_key"

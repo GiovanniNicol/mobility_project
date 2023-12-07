f7ff1f7f9cd0fb34c800f8d4df58da9f2e8bf5cf
kenb10 <152394358+kenb10@users.noreply.github.com>
2023-12-07 18:47:38 +0100

    Update swiss_geoadmin.py

--- utils/swiss_geoadmin.py
+++ utils/swiss_geoadmin.py
@@ -1,51 +1,26 @@
 import requests
 
-def get_closest_vehicle(location):
-    # Step 1: Use GeoAdmin API to get coordinates of the input location
-    geoadmin_api_url = "https://api3.geo.admin.ch/services/sdiservices/search"
-    geoadmin_params = {
-        "query": location,
-        "layer": "ch.bfe.sharedmobility.station",
-        "time": "2022-01-01",  # Adjusting time parameter if needed
-    }
-
-    geoadmin_response = requests.get(geoadmin_api_url, params=geoadmin_params)
-    geoadmin_data = geoadmin_response.json()
-
-    # Check if coordinates are available
-    if "features" not in geoadmin_data or not geoadmin_data["features"]:
-        return "Coordinates not found for the provided location."
-
-    # Extract coordinates from GeoAdmin response
-    coordinates = geoadmin_data["features"][0]["geometry"]["coordinates"]
-    latitude, longitude = coordinates[1], coordinates[0]
-
-    # Step 2: Use sharedmobility.ch API to get the closest vehicle
-    sharedmobility_api_url = "https://api.sharedmobility.ch/v1/sharedmobility/identify"
-    sharedmobility_params = {
+def find_closest_vehicles(latitude, longitude, tolerance):
+    url = "https://api.sharedmobility.ch/v1/sharedmobility/identify"
+    params = {
         "Geometry": f"{longitude},{latitude}",
-        "Tolerance": 500,  # Adjust tolerance parameter if needed
+        "Tolerance": tolerance,
         "offset": 0,
         "geometryFormat": "esrijson",
     }
 
-    sharedmobility_response = requests.get(sharedmobility_api_url, params=sharedmobility_params)
-    sharedmobility_data = sharedmobility_response.json()
+    response = requests.get(url, params=params)
 
-    # Check if vehicles are available
-    if "features" not in sharedmobility_data or not sharedmobility_data["features"]:
-        return "No vehicles found near the provided location."
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
 
-    # Extract information about the closest vehicle
-    closest_vehicle = sharedmobility_data["features"][0]
-    vehicle_type = closest_vehicle["attributes"]["ch.bfe.sharedmobility.vehicle_type"]
-    provider_id = closest_vehicle["attributes"]["ch.bfe.sharedmobility.provider.id"]
+# Get user input
+latitude = float(input("Enter the latitude: "))
+longitude = float(input("Enter the longitude: "))
+tolerance = int(input("Enter the tolerance: "))
 
-    return f"The closest {vehicle_type} from provider {provider_id} is located at {latitude}, {longitude}."
-
-# Example usage
-user_input_location = input("Enter the location: ")
-result = get_closest_vehicle(user_input_location)
+result = find_closest_vehicles(latitude, longitude, tolerance)
 print(result)
-
-

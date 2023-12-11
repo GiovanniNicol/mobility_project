f22463e1dc57f1a1910a5dcf2d1e466ccb656f34
GiovanniNicol <gnicoletti2004@gmail.com>
2023-12-11 10:47:35 +0100

    Update google_maps.py

--- utils/google_maps.py
+++ utils/google_maps.py
@@ -1,9 +1,62 @@
-
 import googlemaps
 from config import GOOGLE_MAPS_API_KEY
 
+
 # Initialize the Google Maps client with your API key
-gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
+# gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
+#
+#
+# def get_geocode(address):
+#     """Get the geocode for a given address."""
+#     try:
+#         result = gmaps.geocode(address)
+#         if not result:
+#             return None, "No results found"
+#         return result, None
+#     except Exception as e:
+#         # Log the error or handle it appropriately
+#         return None, str(e)
+#
+#
+# def calculate_route(origin, destination, mode='driving'):
+#     try:
+#         directions_result = gmaps.directions(origin, destination, mode=mode)
+#         if not directions_result:
+#             return None, "No route found"
+#
+#         # Extract distance and duration
+#         route = directions_result[0]['legs'][0]
+#         distance = route['distance']['text']
+#         duration = route['duration']['text']
+#
+#         return {'distance': distance, 'duration': duration, 'route': directions_result}, None
+#     except Exception as e:
+#         # Improved error logging
+#         return None, f"Error calculating route: {e}"
+
+# Placeholder functions for fetching data from mobility providers
+def fetch_data_from_sbb():
+    # Implement SBB API call and data processing
+    pass
+
+
+def fetch_data_from_tier():
+    # Implement Tier API call and data processing
+    pass
+
+
+def fetch_data_from_bird():
+    # Implement Bird API call and data processing
+    pass
+
+
+def get_mobility_provider_data():
+    """Combine data from all mobility providers."""
+    sbb_data = fetch_data_from_sbb()
+    tier_data = fetch_data_from_tier()
+    bird_data = fetch_data_from_bird()
+    # Combine and process data as needed for your application
+    return combined_data
 
 
 def get_geocode(address):
@@ -12,13 +65,13 @@ def get_geocode(address):
         result = gmaps.geocode(address)
         if not result:
             return None, "No results found"
-        return result, None
+        return result[0]['geometry']['location'], None
     except Exception as e:
-        # Log the error or handle it appropriately
         return None, str(e)
 
 
 def calculate_route(origin, destination, mode='driving'):
+    """Calculate route between two points."""
     try:
         directions_result = gmaps.directions(origin, destination, mode=mode)
         if not directions_result:
@@ -31,5 +84,21 @@ def calculate_route(origin, destination, mode='driving'):
 
         return {'distance': distance, 'duration': duration, 'route': directions_result}, None
     except Exception as e:
-        # Improved error logging
         return None, f"Error calculating route: {e}"
+
+
+def get_transport_options_and_route(user_location, destination):
+    """Get available transport options and calculate route to destination."""
+    mobility_data = get_mobility_provider_data()
+    # Process the mobility data to find available options near the user
+    # ...
+
+    # Calculate the route for each option
+    routes = []
+    for option in mobility_data:
+        origin = option['location']  # Assuming each option has a 'location' field
+        route_info, error = calculate_route(origin, destination)
+        if route_info:
+            routes.append(route_info)
+
+    return routes

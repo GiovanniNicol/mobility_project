19064d671525ae43bd529bbd1650c4e0c476cc35
julietteleroy <61455544+julietteleroy@users.noreply.github.com>
2023-12-14 19:23:48 +0100

    Comments in large cap public_transport.py

--- utils/public_transport.py
+++ utils/public_transport.py
@@ -1,12 +1,12 @@
-# importing requests
+# Importing requests
 import requests
 
-# creating a function to find connections for the public transport
+# Creating a function to find connections for the public transport
 def find_connection(origin, destination, departure_date, departure_time):
 
     api_url = "http://transport.opendata.ch/v1/connections"
 
-    # defining parameters origen, destination, departure date and departure time
+    # Defining parameters origen, destination, departure date and departure time
     params = {
         "from": origin,
         "to": destination,
@@ -14,7 +14,7 @@ def find_connection(origin, destination, departure_date, departure_time):
         "time": departure_time
     }
 
-    # usage of the parameters to find a connection in the api_url and storage in response 
+    # Usage of the parameters to find a connection in the api_url and storage in response 
     response = requests.get(api_url, params=params)
 
     if response.status_code == 200:
@@ -39,7 +39,7 @@ def find_connection(origin, destination, departure_date, departure_time):
 
     else:
 
-        # notification in case of failure
+        # Notification in case of failure
         return f"Failed to retrieve data: {response.status_code}"
 
 

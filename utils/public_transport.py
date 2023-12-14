b47ef0c90794b1179aab4358cdf8a167bc5d7ae6
julietteleroy <61455544+julietteleroy@users.noreply.github.com>
2023-12-14 19:53:28 +0100

    Update public_transport.py

--- utils/public_transport.py
+++ utils/public_transport.py
@@ -1,12 +1,15 @@
-# Importing requests
+# Importing libraries
 import requests
+from datetime import datetime
+from utils import google_maps
 
-# Creating a function to find connections for the public transport
+# Creating a function to find public transport connections
 def find_connection(origin, destination, departure_date, departure_time):
 
+    # SBB public transport API url
     api_url = "http://transport.opendata.ch/v1/connections"
 
-    # Defining parameters origen, destination, departure date and departure time
+    # Scpecifying the parameters for the API request (origin, destination, departure date, and departure time)
     params = {
         "from": origin,
         "to": destination,
@@ -14,15 +17,18 @@ def find_connection(origin, destination, departure_date, departure_time):
         "time": departure_time
     }
 
-    # Usage of the parameters to find a connection in the api_url and storage in response 
+    # Sending a get request to the SBB public transport API url
     response = requests.get(api_url, params=params)
 
+    # Checking whether the get request worked
     if response.status_code == 200:
 
         data = response.json()
 
+        # Getting the information from the first connection in the response
         first_connection = data["connections"][0]
 
+        # Creating a dictionary containing the relevant connection details (latitude, longitude, departure time and platform, arrival time and platform, transport means (public transport lines))
         return_object = {
             "arrival_latitude": first_connection["to"]["station"]["coordinate"]["x"],
             # the mobility provider mixed up y with x
@@ -35,17 +41,19 @@ def find_connection(origin, destination, departure_date, departure_time):
             "transport_means": first_connection["products"],
         }
 
+        # Returning the connection details
         return return_object
 
     else:
 
-        # Notification in case of failure
+        # Specifying an error message if the get request failed
         return f"Failed to retrieve data: {response.status_code}"
 
+# Getting the current date and time
+now = datetime.now()
+depart_date = now.strftime("%Y-%m-%d")
+depart_time = now.strftime("%H:%M")
 
-# now = datetime.now()
-# depart_date = now.strftime("%Y-%m-%d")
-# depart_time = now.strftime("%H:%M")
-#
+# Example use
 # nearest_station_result = google_maps.get_address_from_coordinates(47.432986, 9.375389)
 # print(find_connection("St Gallen", "Zurich", depart_date, depart_time))
